7b4fe5dbbfc8588a75eed41818465d203f1aee7f
Andrey Silva <AndreyFreitag2001@outlook.com>
2021-06-22 12:33:45 -0300

    v1

---
+++ ControleDeTarefas.ProjetoDB/dbo/Tables/TB_contatos.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[TB_contatos] (
+    [ID]       INT           IDENTITY (1, 1) NOT NULL,
+    [EMAIL]    VARCHAR (100) NOT NULL,
+    [TELEFONE] CHAR (11)     NOT NULL,
+    [EMPRESA]  VARCHAR (100) NOT NULL,
+    [CARGO]    VARCHAR (100) NOT NULL,
+    [nome]     VARCHAR (150) NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
---
+++ ControleDeTarefas.ProjetoDB/dbo/Tables/TB_tarefas.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[TB_tarefas] (
+    [ID]                    INT           IDENTITY (1, 1) NOT NULL,
+    [TITULO]                VARCHAR (200) NULL,
+    [PRIORIDADE]            INT           NULL,
+    [DATADECRIACAO]         DATE          NULL,
+    [DATACONCLUSAO]         DATE          NULL,
+    [PERCENTUALDECONCLUSAO] INT           NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
