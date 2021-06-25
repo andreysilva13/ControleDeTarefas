da3fcfab343ca14a88df1a746d42938dcf43ce12
Andrey Silva <AndreyFreitag2001@outlook.com>
2021-06-25 16:22:29 -0300

    v1

---
+++ ControleDeTarefas.ProjetoDB/dbo/Tables/TB_compromisso.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[TB_compromisso] (
+    [ID]          INT           IDENTITY (1, 1) NOT NULL,
+    [ASSUNTO]     VARCHAR (150) NOT NULL,
+    [LUGAR]       VARCHAR (150) NULL,
+    [LINK]        VARCHAR (300) NULL,
+    [DATAINICIO]  SMALLDATETIME NULL,
+    [DATATERMINO] SMALLDATETIME NULL,
+    [CONTATO_ID]  INT           NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_CompromissoContato] FOREIGN KEY ([CONTATO_ID]) REFERENCES [dbo].[TB_contatos] ([ID])
+);
+
